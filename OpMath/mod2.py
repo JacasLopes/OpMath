54b8925aa28060e5db12064b3c0786f8891bcf6e
JacasLopes <joaquim.lopes.duarte@gmail.com>
2021-06-15 15:28:12 +0100

    First Commit by Joaquim Lopes

---
+++ OpMath/mod2.py
@@ -0,0 +1,5 @@
+def say_hi(nome):
+    print(f'Hi, my name is {nome}.')
+
+
+__version__ = '1.1'
---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name="OpMath",
+    version="0.1",
+    description="OpMath Distribution Package",
+    packages=find_packages(),
+    include_package_data=True
+)
